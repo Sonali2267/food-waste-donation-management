aa1ab32b94a49e29456990ce5e4d54ffa19ae504
Sonali2267 <shanumehra988@gmail.com>
2022-09-12 22:18:56 +0530

    Update config.php

--- config.php
+++ config.php
@@ -8,3 +8,11 @@ $dbname="foodwaste";
 
 $conn=mysqli_connect($servername,$username,$password,$dbname);
 
+if(!$conn)
+{
+    die("connection-failed:".mysqli_connect_error());
+}
+else
+{
+    echo "connected successfully";
+}
